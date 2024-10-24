52633570dca562869351d828fafff4e3d1661e9d
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-10-24 10:06:27 +0530

    Leetcode_DailyChallenge

---
+++ Leetcode_DailyChallenge/LeetCode_951.java
@@ -0,0 +1,27 @@
+/*---LEETCODE---DAILY CHALLENGE---
+--------24-10-2024----
+-----LEETCODE 951----
+-----Medium----
+---https://leetcode.com/problems/flip-equivalent-binary-trees/description/----
+
+----JAVA SOLUTION-------
+*/
+
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+
+        if (root1.val == root2.val) {
+            return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) ||
+                   (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+        }
+
+        return false;
+    }
+}
