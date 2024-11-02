77575213fe408140d8cd9bbb8d6457e0c35e9a2d
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-11-02 19:33:29 +0530

    GFG_POTD

---
+++ GFG_POTD/Nov_2024/02_nov_KthDistance.java
@@ -0,0 +1,27 @@
+/*----GFG----
+------POTD----
+----02-11-2024----
+------java_SOLUTION---
+-------Easy----
+--Kth Distance-----
+----https://www.geeksforgeeks.org/problems/kth-distance3757/1----
+
+*/
+
+class Solution {
+    public:
+  
+      bool checkDuplicatesWithinK(vector<int>& arr, int k) {
+          // your code
+          
+          unordered_map<int,int>mp;
+          for(int i=0;i<arr.size();i++){
+              if(mp.find(arr[i])!=mp.end()){
+                  if(i-mp[arr[i]]<=k) return true;
+                  else mp[arr[i]]=i;
+              }
+              mp[arr[i]]=i;
+          }
+          return false;
+      }
+  };
