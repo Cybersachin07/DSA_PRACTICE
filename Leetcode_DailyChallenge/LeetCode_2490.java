de72f39bbc4dbefb071b167b4176d8e6ff33f066
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-11-02 19:27:27 +0530

    Leetcode_DailyChallenge

---
+++ Leetcode_DailyChallenge/LeetCode_2490.java
@@ -0,0 +1,29 @@
+/*---LEETCODE---DAILY CHALLENGE---
+-------02-11-2024----
+-----LEETCODE 2490----
+-----Easy----
+---
+----JAVA SOLUTION-------
+*/
+
+
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        if (sentence.charAt(sentence.length() - 1) != sentence.charAt(0)) {
+            return false;
+        }
+
+        int n = sentence.length();
+        int i = 0;
+        while (i < n) {
+            while (i < n && sentence.charAt(i) != ' ') {
+                i++;
+            }
+            if (i < n && sentence.charAt(i - 1) != sentence.charAt(i + 1)) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+}
