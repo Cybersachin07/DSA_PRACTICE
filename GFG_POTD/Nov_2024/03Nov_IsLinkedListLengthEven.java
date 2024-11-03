323e32ce65f26db0239040075e4280ffa34cc789
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-11-03 13:14:05 +0530

    GFG_POTD

---
+++ GFG_POTD/Nov_2024/03Nov_IsLinkedListLengthEven.java
@@ -0,0 +1,26 @@
+/*----GFG----
+------POTD----
+----03-11-2024----
+------cpp_SOLUTION---
+-------Easy----
+--Is Linked LIst Length Even-----
+----https://www.geeksforgeeks.org/problems/linked-list-length-even-or-odd/1----
+
+*/
+
+class Solution {
+    public boolean isLengthEven(Node head) {
+        // code here
+        
+        Node temp = head;
+        int count = 0;
+        while( temp != null){
+            count ++;
+            temp = temp.next;
+            
+        }
+        if(count%2==0)return true;
+        return false;
+    }
+}
+
