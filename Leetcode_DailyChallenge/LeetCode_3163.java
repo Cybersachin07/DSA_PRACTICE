bd3ef5f97930a816c2c91d66f69041bbed46fdc5
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-11-04 12:57:46 +0530

    Leetcode_DailyChallenge

---
+++ Leetcode_DailyChallenge/LeetCode_3163.java
@@ -0,0 +1,30 @@
+/*---LEETCODE---DAILY CHALLENGE---
+--------04-11-2024----
+-----LEETCODE 3163----
+-----Medium---
+---https://leetcode.com/problems/string-compression-iii/----
+
+----JAVA SOLUTION-------
+*/
+
+class Solution {
+    public String compressedString(String word) {
+        StringBuilder comp = new StringBuilder();
+        int n = word.length();
+        int i = 0;
+
+        while (i < n) {
+            int count = 0;
+            char ch = word.charAt(i);
+
+            while (i < n && count < 9 && word.charAt(i) == ch) {
+                count++;
+                i++;
+            }
+
+            comp.append(count).append(ch);
+        }
+
+        return comp.toString();
+    }
+}
