e95f11e5409eb6546ed32e1a8565419b9c97790f
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-11-07 12:51:35 +0530

    Leetcode_DailyChallenge

---
+++ Leetcode_DailyChallenge/LeetCode_2275.java
@@ -0,0 +1,28 @@
+/*---LEETCODE---DAILY CHALLENGE---
+--------06-11-2024----
+-----LEETCODE 2275--------
+----Medium----
+-------
+
+----JAVA SOLUTION-------
+*/
+
+class Solution {
+    public int largestCombination(int[] candidates) {
+        int result = 0;
+
+        for (int i = 0; i < 24; i++) { // O(24)
+            int countIth = 0;
+
+            for (int num : candidates) { // O(n)
+                if ((num & (1 << i)) != 0) {
+                    countIth++;
+                }
+            }
+
+            result = Math.max(result, countIth);
+        }
+
+        return result;
+    }
+}
