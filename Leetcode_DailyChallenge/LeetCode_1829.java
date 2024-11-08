8b60d35373f4bd40e344c6b041f11fc159503d26
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-11-08 11:53:42 +0530

    Leetcode_DailyChallenge

---
+++ Leetcode_DailyChallenge/LeetCode_1829.java
@@ -0,0 +1,36 @@
+/*---LEETCODE---DAILY CHALLENGE---
+--------08-11-2024----
+-----LEETCODE 1829----
+-----Medium----
+-------
+
+----JAVA SOLUTION-------
+*/
+
+
+class Solution {
+    public int[] getMaximumXor(int[] nums, int maximumBit) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        
+        int XOR = 0;
+        for (int num : nums) {
+            XOR ^= num;
+        }
+
+        
+        int mask = (1 << maximumBit) - 1;
+
+       
+        for (int i = 0; i < n; i++) {
+            
+            result[i] = XOR ^ mask;
+
+            
+            XOR ^= nums[n - 1 - i];
+        }
+
+        return result;
+    }
+}
