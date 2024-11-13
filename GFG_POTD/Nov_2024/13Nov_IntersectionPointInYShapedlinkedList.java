34c41b9e2ff69fdf438811a2411463a5efd38bac
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-11-13 20:13:20 +0530

    GFG_POTD

--- GFG_POTD/Nov_2024/13Nov_IntersectionPointInYShapedlinkedList.java
+++ GFG_POTD/Nov_2024/13Nov_IntersectionPointInYShapedlinkedList.java
@@ -1,6 +1,44 @@
+/*----GFG----
+------POTD----
+----12-11-2024----
+------java_SOLUTION---
+-------Medium----
+----Intersection Point In Y shaped Linked List-----
+https://www.geeksforgeeks.org/problems/intersection-point-in-y-shapped-linked-lists/1
+*/
 
-
-
+class Intersect {
+    // Function to find intersection point in Y shaped Linked Lists.
+     static int findLength(Node head){
+        Node temp=head;
+        int len=0;
+        while(temp!=null){
+            len++;
+            temp=temp.next;
+        }
+        return len;
+    }
+   
+    int intersectPoint(Node head1, Node head2) {
+        // code here
+        int len1=findLength(head1);
+        int len2=findLength(head2);
+        while(len1>len2){
+            head1=head1.next;
+            len1--;
+        }
+        while(len2>len1){
+            head2=head2.next;
+            len2--;
+        }
+        while(head1!=null && head2!=null){
+            if(head1==head2)return head1.data;
+            head1=head1.next;
+            head2=head2.next;
+        }
+        return -1;
+    }
+}
 
 
 /*-------------cpp solution=-------- */
