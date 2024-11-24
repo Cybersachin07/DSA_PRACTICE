dae3da6961c94769218431970230e859673dfe39
Cybersachin07 <114096795+Cybersachin07@users.noreply.github.com>
2024-11-24 14:43:44 +0530

    GFG_POTD

---
+++ GFG_POTD/Nov_2024/24Nov_KadensAlgo.java
@@ -0,0 +1,28 @@
+/*----GFG----
+------POTD----
+----24-11-2024----
+------java_SOLUTION---
+------Medium----
+----Kadens Algo----
+https://www.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+*/
+
+
+class Solution {
+
+    // arr: input array
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int[] arr) {
+
+        // Your code here
+        
+        int sum=0,ans=Integer.MIN_VALUE,n=arr.length;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            ans=Math.max(ans,sum);
+            if(sum<0)sum=0;
+        }
+        return ans;
+    
+    }
+}
